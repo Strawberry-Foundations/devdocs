9295010b2831b9704af81af87c461646f52bbc81
matteodev8 <59450459+matteodev8@users.noreply.github.com>
2023-11-12 06:38:12 +0100

    Added dev start script

---
+++ dev.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+echo "This script requires the mintlify CLI application installed via npm or yarn"
+echo "For this, run 'npm install -g mintlify' (Note: You might need to add sudo on Linux systems)"
+mintlify dev
